882e7103f0e91ebbe2228b12674a266acbc47de8
mymint01 <captainchuenthavorn@gmail.com>
2020-08-18 19:54:55 +0700

    complete 0001

--- ProgrammingTH/0001.cpp
+++ ProgrammingTH/0001.cpp
@@ -1,1 +1,17 @@
 #include<stdio.h>
+int main() {
+	int a, b, sum;
+	scanf("%d", a);
+	scanf("%d", b);
+	sum = a + b;
+	if (sum > 79)	printf("A");
+	else if (sum > 74 && sum<=100)	printf("B+");
+	else if (sum > 69)	printf("B");
+	else if (sum > 64)	printf("C+");
+	else if (sum > 59)	printf("C");
+	else if (sum > 54)	printf("D+");
+	else if (sum > 49)	printf("D");
+	else if (sum >= 0)	printf("F");
+	
+		return 0
+}

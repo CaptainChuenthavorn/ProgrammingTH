dedce783671d05843fb42c7ba9d752220899abcb
จักรภัทรณ์ ชื่นถาวร <captainchuenthavorn@hotmail.com>
2020-08-26 17:52:25 +0700

    finish

--- ProgrammingTH/0003.cpp
+++ ProgrammingTH/0003.cpp
@@ -1,5 +1,5 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include<stdio.h>
+/*#include<stdio.h>
 int main() {
 	long int  i,k,l,j,m, n,matrixA[100],matrixB[100],sum[100];
 	scanf("%d", &m);
@@ -31,4 +31,31 @@ int main() {
 	}
 	
 	return 0;
+}*/
+
+#include<stdio.h>
+int main() {
+	int matrixA[100][100], matrixB[100][100], m, n, count,a,b;
+	scanf("%d %d", &m, &n);
+	for (a = 0;a < m;a++) {
+		for (b = 0;b < n;b++) {
+
+			scanf("%d", &matrixA[a][b]);
+		}
+	}
+	for (a = 0;a < m;a++) {
+		for (b = 0;b < n;b++) {
+
+			scanf("%d", &matrixB[a][b]);
+		}
+	}
+	for (a = 0;a < m;a++) {
+		for (b = 0;b < n;b++) {
+
+			printf("%d ", matrixA[a][b] + matrixB[a][b]);
+		}
+		printf("\n");
+	}
+	return 0;
+
 }

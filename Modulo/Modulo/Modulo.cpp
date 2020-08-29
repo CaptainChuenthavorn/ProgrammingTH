20bce10acd263f5813de725634edac4cb1a3235f
จักรภัทรณ์ ชื่นถาวร <captainchuenthavorn@hotmail.com>
2020-08-29 11:40:09 +0700

    finish

--- Modulo/Modulo/Modulo.cpp
+++ Modulo/Modulo/Modulo.cpp
@@ -4,23 +4,25 @@ int main()
 {
 	int input[10], count[41], i,ans, k;
 	ans = 0;
-	printf("Enter 10 number : \n");
+	printf("Enter 10 number  ");
 	for (i = 0;i < 10;i++) {
+		printf("\nEnter number %d : ",i+1);
 		scanf("%d", &input[i]);
 		k = input[i] % 42;
+		printf("Mod : %d %% 42 = %d\n", input[i], k);
 		count[k] = 1;
 	}
 
-	for (i = 0;i < 10;i++) {
-		printf("\n%d %% 42 = %d", input[i], count[k]);
-	}
-	
+	printf("************\n");
+	printf("There are have number :");
 	for (k = 0;k < 42;k++) {
 		if (count[k] == 1) {
-			printf("There are have number :");
+			
+			printf("\n%d", k);
 			ans++;
 		}
 	}
+	printf("\n************");
 	printf("\nThe number of answer is %d", ans);
 	return 0;
 }

cb7aa4e34db61f626e75d9f800858b49eed7fb23
จักรภัทรณ์ ชื่นถาวร <captainchuenthavorn@hotmail.com>
2020-09-01 19:27:54 +0700

    add logic

--- Jelly/Jelly/jelly.cpp
+++ Jelly/Jelly/jelly.cpp
@@ -2,7 +2,26 @@
 #include <stdio.h>
 
 int main() {
-	int x[3],i,k;
-	for()
-	scanf()
+	int x[3],i,k,count;
+	count = 0;
+	for (i = 0;i < 3;i++) {
+		scanf("%d", &x[i]);
+
+	}
+	while (x[0] != 1 && x[1] != 1 && x[2] != 1) {
+		for (k = 1;k < 2;i++) {
+			for (i = 0;i < 3;i++) {
+				if (x[i] > x[k])
+				{
+					printf("max : %d", x[i]);
+
+					break;
+				}
+			}
+		}
+		x[i] /= 2;
+		count++;
+		printf("%d", count);
+	}
+	printf("%d", count);
 }

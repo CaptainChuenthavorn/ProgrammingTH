bcddcaeb2e069ac6ca91313cdac9e78ad7b452c5
จักรภัทรณ์ ชื่นถาวร <captainchuenthavorn@hotmail.com>
2020-09-03 17:42:02 +0700

    add interface

--- Jelly/Jelly/jelly.cpp
+++ Jelly/Jelly/jelly.cpp
@@ -2,26 +2,30 @@
 #include <stdio.h>
 
 int main() {
-	int x[3],i,k,count;
+	int x[3],i,count;
 	count = 0;
 	for (i = 0;i < 3;i++) {
 		scanf("%d", &x[i]);
-
-	}
-	while (x[0] != 1 && x[1] != 1 && x[2] != 1) {
-		for (k = 1;k < 2;i++) {
-			for (i = 0;i < 3;i++) {
-				if (x[i] > x[k])
-				{
-					printf("max : %d", x[i]);
-
-					break;
-				}
+	}	
+		for (i = 0;i < 3;i++) {
+			if (x[i] > 0 && x[i] < 1000000) {
+				while (x[i] != 1) {
+					if (x[i] % 2 == 1) {
+						x[i] = x[i] - 1;
+						printf("*Cut down*\n");
 			}
-		}
-		x[i] /= 2;
-		count++;
-		printf("%d", count);
+				x[i] /= 2;
+				count++;
+				printf("jelly [%d,%d,%d]  count +1\n", x[0], x[1], x[2]);
+			}
+			
+			}
+			else {
+				printf("ERROR");
+				break;
+			}
+			
 	}
-	printf("%d", count);
+		printf("\ncount = %d", count);
+		return 0 ;
 }
